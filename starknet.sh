6942c26fec5597516e7cb350e893dc1abf2517cb
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-05-02 01:09:30 +0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-7c62b5fc-e25e-448a-862a-1a82dccd1a93/"
 
 OUTPUT_DIR="logs"
 
